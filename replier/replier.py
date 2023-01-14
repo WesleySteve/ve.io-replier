df34b6c2fbbea790559517b802f48373f99baabc
Wesley Steve <wesley.silva23@hotmail.com>
2023-01-14 05:55:16 -0300

    feat: replier, initial layout from page

--- replier/replier.py
+++ replier/replier.py
@@ -8,3 +8,14 @@ import PySimpleGUI as sg
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+# list files in the directory
+list_files = []
+
+layout = [
+    [sg.Button('Search Files')],
+    [sg.ListBox(list_files, size=(70,30), key='-LIST-')]
+    
+]
+
+
