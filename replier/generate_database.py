f4e6bece14ea1893e0eca92f0860451561519347
Wesley Steve <wesley.silva23@hotmail.com>
2023-01-14 02:40:02 -0300

    feat: generate_database, defined page layout and added verification of uploaded file

--- replier/generate_database.py
+++ replier/generate_database.py
@@ -5,7 +5,34 @@ from upload_data.upload_data_sqlite import check_db_exists, create_db_files_xlsx
 
 
 # project paths
-BASE_DIR = os.path.dirname(os.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
+
+layout = [
+    [sg.Text('Choose file'), sg.Input(), sg.FileBrowse(key='-IN-')],
+    [sg.Button('Submit')],
+    [sg.Text(key='-FILE_PATH-')],
+    [sg.Text(key='-MSG-')]
+  
+]
+
+window = sg.Window('System Replier', layout, size=(1200,300))
+
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    
+    if event == 'Submit':
+        
+        path = values['-IN-']
+        file = os.path.basename(path)
+        
+        window['-FILE_PATH-'].update(f'O caminho do arquivo {path}')
+        
+
+window.close()    
