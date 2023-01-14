f4ca681118343b482ac5d51a2050e0e50466e9b4
Wesley Steve <wesley.silva23@hotmail.com>
2023-01-14 02:07:51 -0300

    chore: defined project paths

--- replier/generate_database.py
+++ replier/generate_database.py
@@ -5,3 +5,7 @@ from upload_data.upload_data_sqlite import check_db_exists, create_db_files_xlsx
 
 
 # project paths
+BASE_DIR = os.path.dirname(os.dirname(os.path.abspath(__file__)))
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
